6d9955125ee5e82ff1391d5bfa36be0610e87d2b
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-07-02 21:03:53 +1000

    Add aggregation pipeline (#5)

--- src/functions/syncMempool.ts
+++ src/functions/syncMempool.ts
@@ -3,6 +3,6 @@ import { syncMempool } from "../jobs/syncMempool.js"
 import { ContextLogger } from "../utils/Logger.js"
 
 app.timer('syncMempool', {
-  schedule: '0 */1 * * * *',
+  schedule: '0 * * * * *',
   handler: async (_, context) => { await syncMempool(new ContextLogger(context)) }
 })
--- src/jobs/syncMempool.ts
+++ src/jobs/syncMempool.ts
@@ -1,5 +1,6 @@
 import { Transaction } from "bitcoinjs-lib"
 import { database } from "../database/database.js"
+import { syncPendingMints } from "../database/syncPendingMints.js"
 import { decodeAlkaneOpCallsInTransaction } from "../utils/decoder.js"
 import { Logger } from "../utils/Logger.js"
 import { getMempoolTransactionIds } from "../utils/rpc/getMempoolTransactionIds.js"
@@ -12,17 +13,12 @@ export async function syncMempool(log: Logger) {
   log.info(`Found ${mempoolTxIds.length.toString()} transactions in the mempool.`)
   const mempoolTxIdsSet = new Set(mempoolTxIds)
 
-  const dbTxIds = (await database.mempoolTransaction.find().toArray()).map(tx => tx.txid)
-  const dbTxIdsSet = new Set(dbTxIds)
+  const dbTxns = await database.mempoolTransaction.find().toArray()
+  const dbTxnMintMap = new Map(dbTxns.map(tx => [tx.txid, tx.mintId]))
   
-  const newTxns = mempoolTxIds.filter(txid => !dbTxIdsSet.has(txid))
+  const newTxns = mempoolTxIds.filter(txid => !dbTxnMintMap.has(txid))
 
-  const txnsToDelete = dbTxIds.filter(txid => !mempoolTxIdsSet.has(txid))
-
-  if (txnsToDelete.length > 0) {
-    await database.mempoolTransaction.deleteMany({ txid: { $in: txnsToDelete } })
-  }
-  log.info(`Deleted ${txnsToDelete.length.toString()} transactions from the database.`)
+  const txnsToDelete = dbTxns.filter(txn => !mempoolTxIdsSet.has(txn.txid))
 
   const mempoolTransactions = newTxns.length === 0 ? [] : (
     await getRawTransactions(newTxns.slice(0, MAX_TXNS_PER_SYNC))).filter(x => x.success)
@@ -34,10 +30,28 @@ export async function syncMempool(log: Logger) {
       }
     )
 
-  if (mempoolTransactions.length > 0) {
-    await database.mempoolTransaction.insertMany(mempoolTransactions)
-  }
-  log.info(`Inserted ${mempoolTransactions.length.toString()} new transactions into the database.`)
+  const deletedAlkanes = txnsToDelete.map(x => x.mintId).filter(x => x != null)
+  const newAlkanes = mempoolTransactions.map(x => x.mintId).filter(x => x != null)
+  const modifiedAlkanes = new Set(deletedAlkanes.concat(newAlkanes))
+
+  await database.withTransaction(async () => {
+    if (txnsToDelete.length > 0) {
+      await database.mempoolTransaction.deleteMany({ txid: { $in: txnsToDelete.map(x => x.txid) } })
+    }
+    log.info(`Deleted ${txnsToDelete.length.toString()} transactions from the database.`)
+
+    if (mempoolTransactions.length > 0) {
+      await database.mempoolTransaction.insertMany(mempoolTransactions)
+    }
+    log.info(`Inserted ${mempoolTransactions.length.toString()} new transactions into the database.`)
+
+    if (modifiedAlkanes.size > 0) {
+      log.info(`Syncing ${modifiedAlkanes.size.toString()} alkanes due to mempool changes.`)
+      await syncPendingMints({ alkaneId: { $in: Array.from(modifiedAlkanes) } })
+    } else {
+      log.info("No alkanes to sync from mempool changes.")
+    }
+  })
 
   return {
     deletedCount: txnsToDelete.length,
