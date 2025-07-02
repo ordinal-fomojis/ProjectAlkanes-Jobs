663cc17b2472c7a1f6908c6485c0fdb93f14130f
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-07-02 21:18:30 +1000

    fix name

--- src/functions/syncPendingMints.ts
+++ src/functions/syncPendingMints.ts
@@ -1,7 +1,7 @@
 import { app } from "@azure/functions"
 import { syncPendingMints } from "../database/syncPendingMints.js"
 
-app.timer('syncMempool', {
+app.timer('syncPendingMints', {
   schedule: '0 0 * * * *',
   handler: async () => { await syncPendingMints(null) }
 })
