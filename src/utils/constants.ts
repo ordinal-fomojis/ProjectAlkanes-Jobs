57024e866435d323de80bfdc665894cdf7c0e989
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-08-31 10:28:58 +1000

    add non prod env file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,14 @@ import { config } from '@dotenvx/dotenvx'
 import { z } from 'zod'
 import { parse } from '../utils/parse.js'
 
-config({ path: '.env.production', quiet: true })
+export const ENV = parse(z.enum(['production', 'development', 'test']).default('development'), process.env.NODE_ENV)
+
+config({
+  path: ENV === 'test'
+    ? '.env.sample'
+    : process.env.DOTENV_PATH,
+  quiet: true
+})
 
 export type BitcoinNetwork = typeof BITCOIN_NETWORK
 export const BITCOIN_NETWORK = parse(z.enum(['mainnet', 'signet', 'testnet'])
