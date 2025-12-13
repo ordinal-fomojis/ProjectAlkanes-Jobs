9c9a4ef1e0cd6c5bb12cc6b37ba1be091ec13035
Vannix <134819232+Vanniix@users.noreply.github.com>
2025-12-13 11:24:24 +1100

    hex encode standard brc tickers in api requests (#94)

--- src/utils/brc-ticker.ts
+++ src/utils/brc-ticker.ts
@@ -14,3 +14,14 @@ export function tickerLength(ticker: string) {
 export function brcType(ticker: string) {
   return tickerLength(ticker) === 6 ? BrcType.SixByte : BrcType.Default
 }
+
+// Unisat API has issues with standard URL encoding (e.g. fails with %2F for '/')
+// However, the standard API accepts hex-encoded tickers, so we use that instead to allow for tickers with special characters.
+// For 6 byte tickers, the API doesn't support this, but six byte tickers are always alphabetic, so we can just use them as is.
+// This is how the Unisat front end handles tickers in API requests.
+export function tickerToPathQuery(ticker: string) {
+  const bytes = Buffer.from(ticker, 'utf-8')
+  if (bytes.length === 6)
+    return ticker.toLowerCase()
+  return bytes.toString('hex')
+}
